e26fd7d81dc983fbb4a28402d58ade89f8cff62a
Michal Majer <misa@majer.cz>
2022-03-02 21:56:59 +0100

    thread communication, camera movement

--- slint_app/src/main.rs
+++ slint_app/src/main.rs
@@ -1,124 +1,20 @@
 use std::sync::{Arc, Mutex};
 
-use nalgebra::{point, vector};
-use raycaster_lib::{
-    camera::PerspectiveCamera,
-    premade::{
-        parse::{from_file, skull_parser},
-        transfer_functions::skull_tf,
-    },
-    render::{RenderOptions, Renderer},
-    volumetric::LinearVolume,
+use render_thread::{
+    RenderThread, RenderThreadMessage, RENDER_HEIGHT, RENDER_HEIGHT_U, RENDER_WIDTH, RENDER_WIDTH_U,
 };
 use slint::{Image, Rgb8Pixel, SharedPixelBuffer};
 
 slint::include_modules!();
 
-const RENDER_WIDTH_U: usize = 700;
-const RENDER_HEIGHT_U: usize = 700;
+mod render_thread;
 
-const RENDER_WIDTH: u32 = RENDER_WIDTH_U as u32;
-const RENDER_HEIGHT: u32 = RENDER_HEIGHT_U as u32;
+/* chybí mousewheel
+// y        ... vertical scroll
+                // +1 unit  ... 1 step of wheel down (negative -> scroll up)
 
-// pub struct State {
-//     pub left_mouse_held: bool,
-//     pub right_mouse_held: bool,
-// }
-
-// impl State {
-//     fn get_user_input(&mut self, cam: &mut PerspectiveCamera, event: &sdl2::event::Event) {
-//         match event {
-//             Event::MouseMotion { xrel, yrel, .. } => {
-//                 // When mouse button is down, drag camera around
-
-//                 match (self.left_mouse_held, self.right_mouse_held) {
-//                     (false, false) => (),
-//                     (true, false) => {
-//                         // move on the plane described by camera position and normal
-//                         let drag_diff = (*xrel as f32, *yrel as f32);
-//                         cam.change_pos_plane(-drag_diff.0 * 1.2, -drag_diff.1 * 1.2);
-//                     }
-//                     (false, true) => {
-//                         // change camera direction
-//                         let drag_diff = (*xrel as f32, *yrel as f32);
-//                         cam.look_around(drag_diff.0 * -0.01, drag_diff.1 * -0.01);
-//                     }
-//                     (true, true) => {
-//                         // rotate around origin
-//                         let drag_diff = (*xrel as f32, *yrel as f32);
-//                         let axisangle = Vector3::y() * (std::f32::consts::FRAC_PI_8 * drag_diff.0);
-//                         let rot = nalgebra::Rotation3::new(axisangle);
-
-//                         cam.change_pos_matrix(rot);
-//                     }
-//                 }
-//             }
-//             Event::MouseButtonDown { mouse_btn, .. } => match mouse_btn {
-//                 sdl2::mouse::MouseButton::Left => self.left_mouse_held = true,
-//                 sdl2::mouse::MouseButton::Right => self.right_mouse_held = true,
-//                 _ => (),
-//             },
-//             Event::MouseButtonUp { mouse_btn, .. } => match mouse_btn {
-//                 sdl2::mouse::MouseButton::Left => self.left_mouse_held = false,
-//                 sdl2::mouse::MouseButton::Right => self.right_mouse_held = false,
-//                 _ => (),
-//             },
-//             Event::MouseWheel { y, .. } => {
-//                 // y        ... vertical scroll
-//                 // +1 unit  ... 1 step of wheel down (negative -> scroll up)
-
-//                 cam.change_pos_view_dir((*y as f32) * 5.0);
-//             }
-//             _ => {}
-//         }
-//     }
-// }
-
-fn start_render_thread(
-    app_weak: slint::Weak<App>,
-    shared_buffer: Arc<Mutex<Vec<u8>>>,
-    rx: std::sync::mpsc::Receiver<bool>,
-) -> std::thread::JoinHandle<()> {
-    std::thread::spawn(move || {
-        let volume: LinearVolume = from_file("volumes/Skull.vol", skull_parser, skull_tf).unwrap();
-
-        let pos = point![300.0, 300.0, 300.0];
-        let dir = vector![-1.0, -1.0, -1.0];
-        let camera = PerspectiveCamera::new(pos, dir);
-
-        let mut raycast_renderer = Renderer::<_, _>::new(volume, camera);
-
-        raycast_renderer.set_render_options(RenderOptions {
-            resolution: (RENDER_WIDTH_U, RENDER_HEIGHT_U),
-            ray_termination: true,
-            empty_index: true,
-            multi_thread: false,
-        });
-
-        loop {
-            raycast_renderer
-                .camera
-                .change_pos(vector![10.0, 10.0, 10.0]); // canary
-            {
-                let mut lock = shared_buffer.lock().unwrap();
-                raycast_renderer.render_to_buffer(lock.as_mut_slice());
-                // drop of mutex
-            }
-
-            let handle_copy = app_weak.clone();
-            slint::invoke_from_event_loop(move || {
-                handle_copy.unwrap().invoke_send_rendered_frame()
-            });
-
-            let res = rx.recv().unwrap();
-            if res {
-                continue;
-            } else {
-                break;
-            }
-        }
-    })
-}
+                cam.change_pos_view_dir((*y as f32) * 5.0);
+                 */
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
@@ -131,18 +27,19 @@ pub fn main() {
     let app = App::new();
     let app_thread = app.as_weak();
     let app_render = app_thread.clone();
-    let app_move = app_thread.clone();
 
     // IPC
-    let (tx, rx) = std::sync::mpsc::channel();
     let shared_img = Arc::new(Mutex::new(vec![0u8; 3 * RENDER_WIDTH_U * RENDER_HEIGHT_U]));
     let shared_img_thread = shared_img.clone();
 
     // Rendering thread
-    let render_thread_handle = start_render_thread(app_thread, shared_img_thread, rx);
+    let render_thread = RenderThread::new(app_thread, shared_img_thread);
+    let renderer_sender = render_thread.get_sender();
+    let render_thread_handle = render_thread.start();
 
     // Callback
     // Invoked when new frame is rendered
+    let sender = renderer_sender.clone();
     app.on_send_rendered_frame(move || {
         let app = app_render.unwrap();
 
@@ -155,23 +52,29 @@ pub fn main() {
                 .clone_from_slice(lock.as_slice());
             // mutex drop
         }
-        tx.send(true).unwrap();
+        sender.send_message(RenderThreadMessage::StartRendering);
         let image = Image::from_rgb8(pixel_buffer);
         app.set_render_target(image);
     });
 
+    // React to mouse move in render area
+    let sender = renderer_sender.clone();
+    app.on_render_area_move_event(move |mouse_pos| {
+        sender.send_message(RenderThreadMessage::MousePos(mouse_pos));
+    });
+
+    // React to mouse event (click) in render area
+    let sender = renderer_sender.clone();
     app.on_render_area_pointer_event(move |pe| {
-        println!("Pointer");
-        println!("{pe:?}");
+        sender.send_message(RenderThreadMessage::MouseClick(pe));
     });
 
-    app.on_render_area_moved_event(move || {
-        let app = app_move.unwrap();
-        println!("Move");
-        let x = app.get_render_area_mouse_x();
-        println!("-- {:?}", x);
-    });
+    app.show();
+    slint::run_event_loop();
+    app.hide();
 
-    app.run();
+    println!("App shutting down");
+    renderer_sender.send_message(RenderThreadMessage::ShutDown);
+
     render_thread_handle.join().unwrap();
 }
---
+++ slint_app/src/render_thread.rs
@@ -0,0 +1,211 @@
+use std::sync::{Arc, Mutex};
+
+use crossbeam_channel::{Receiver, Sender};
+use nalgebra::{point, vector, Vector3};
+use raycaster_lib::{
+    camera::PerspectiveCamera,
+    premade::{
+        parse::{from_file, skull_parser},
+        transfer_functions::skull_tf,
+    },
+    render::{RenderOptions, Renderer},
+    volumetric::LinearVolume,
+};
+use slint::re_exports::{PointerEvent, PointerEventButton, PointerEventKind};
+
+use crate::{App, MousePos}; // todo
+
+pub const RENDER_WIDTH_U: usize = 700;
+pub const RENDER_HEIGHT_U: usize = 700;
+
+pub const RENDER_WIDTH: u32 = RENDER_WIDTH_U as u32;
+pub const RENDER_HEIGHT: u32 = RENDER_HEIGHT_U as u32;
+
+pub struct State {
+    pub can_start_rendering: bool,
+    pub left_mouse_held: bool,
+    pub right_mouse_held: bool,
+    pub mouse_x: f32,
+    pub mouse_y: f32,
+}
+
+impl State {
+    fn new() -> State {
+        State {
+            can_start_rendering: true,
+            left_mouse_held: false,
+            right_mouse_held: false,
+            mouse_x: 0.0,
+            mouse_y: 0.0,
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum RenderThreadMessage {
+    StartRendering,
+    ChangeResolution(usize, usize),
+    MousePos(MousePos),
+    MouseClick(PointerEvent),
+    ShutDown,
+}
+
+#[derive(Clone)]
+pub struct RenderThreadMessageSender(Sender<RenderThreadMessage>);
+
+impl RenderThreadMessageSender {
+    pub fn new(sender: Sender<RenderThreadMessage>) -> Self {
+        RenderThreadMessageSender(sender)
+    }
+
+    pub fn send_message(&self, message: RenderThreadMessage) {
+        self.0
+            .send(message)
+            .expect("Cannot send message to render thread");
+    }
+}
+
+pub struct RenderThread {
+    state: State,
+    app_weak: slint::Weak<App>,
+    shared_buffer: Arc<Mutex<Vec<u8>>>,
+    message_sender: Sender<RenderThreadMessage>,
+    message_receiver: Receiver<RenderThreadMessage>, // todo handle all inputs, THEN start rendering
+}
+
+impl RenderThread {
+    pub fn new(app_weak: slint::Weak<App>, shared_buffer: Arc<Mutex<Vec<u8>>>) -> Self {
+        let (message_sender, message_receiver) = crossbeam_channel::unbounded();
+        Self {
+            state: State::new(),
+            app_weak,
+            shared_buffer,
+            message_sender,
+            message_receiver,
+        }
+    }
+
+    pub fn get_sender(&self) -> RenderThreadMessageSender {
+        RenderThreadMessageSender(self.message_sender.clone())
+    }
+
+    pub fn start(mut self) -> std::thread::JoinHandle<()> {
+        std::thread::spawn(move || {
+            let volume: LinearVolume =
+                from_file("volumes/Skull.vol", skull_parser, skull_tf).unwrap();
+
+            let pos = point![300.0, 300.0, 300.0];
+            let dir = vector![-1.0, -1.0, -1.0];
+            let camera = PerspectiveCamera::new(pos, dir);
+
+            let mut raycast_renderer = Renderer::<_, _>::new(volume, camera);
+
+            raycast_renderer.set_render_options(RenderOptions {
+                resolution: (RENDER_WIDTH_U, RENDER_HEIGHT_U),
+                ray_termination: true,
+                empty_index: true,
+                multi_thread: false,
+            });
+
+            loop {
+                {
+                    let mut lock = self.shared_buffer.lock().unwrap();
+                    raycast_renderer.render_to_buffer(lock.as_mut_slice());
+                    // drop of mutex
+                }
+
+                self.notify_frame_rendered();
+
+                let res = self.get_user_input(&mut raycast_renderer.camera);
+                if res {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        })
+    }
+
+    // todo can be replaced with mpsc
+    fn notify_frame_rendered(&mut self) {
+        let handle_copy = self.app_weak.clone();
+        self.state.can_start_rendering = false;
+        slint::invoke_from_event_loop(move || handle_copy.unwrap().invoke_send_rendered_frame());
+    }
+
+    // todo dont use bool
+    // todo build translation matrix in different thread, just apply it here and continue rendering
+    fn get_user_input(&mut self, cam: &mut PerspectiveCamera) -> bool {
+        loop {
+            let event = self.message_receiver.try_recv();
+
+            let event = match event {
+                Ok(e) => e,
+                Err(_) => {
+                    // no more commands
+                    if self.state.can_start_rendering {
+                        return true;
+                    } else {
+                        continue; // spin
+                    }
+                }
+            };
+
+            match event {
+                RenderThreadMessage::StartRendering => self.state.can_start_rendering = true,
+                RenderThreadMessage::ChangeResolution(w, h) => todo!(),
+                RenderThreadMessage::MouseClick(pe) => self.handle_pointer_event(pe),
+                RenderThreadMessage::MousePos(m) => self.handle_mouse_pos(cam, m),
+                RenderThreadMessage::ShutDown => return false,
+            }
+        }
+    }
+
+    fn handle_mouse_pos(&mut self, cam: &mut PerspectiveCamera, action: MousePos) {
+        // rust-analyzer struggles here because m is of generated type
+        // The type is (f32, f32)
+        let drag_diff = (self.state.mouse_x - action.x, self.state.mouse_y - action.y); // todo reset mouse_xy when click
+        self.state.mouse_x = action.x;
+        self.state.mouse_y = action.y;
+        match (self.state.left_mouse_held, self.state.right_mouse_held) {
+            (false, false) => (),
+            (true, false) => {
+                // move on the plane described by camera position and normal
+                cam.change_pos_plane(drag_diff.0 * 0.2, drag_diff.1 * 0.2);
+            }
+            (false, true) => {
+                // change camera direction
+                cam.look_around(drag_diff.0 * -0.001, drag_diff.1 * -0.001);
+            }
+            (true, true) => {
+                // rotate around origin
+                let axisangle = Vector3::y() * (std::f32::consts::FRAC_PI_8 * drag_diff.0);
+                let rot = nalgebra::Rotation3::new(axisangle);
+
+                cam.change_pos_matrix(rot);
+            }
+        }
+    }
+
+    fn handle_pointer_event(&mut self, pe: PointerEvent) {
+        match pe {
+            PointerEvent {
+                button: PointerEventButton::left,
+                kind: PointerEventKind::up,
+            } => self.state.left_mouse_held = false,
+            PointerEvent {
+                button: PointerEventButton::left,
+                kind: PointerEventKind::down,
+            } => self.state.left_mouse_held = true,
+            PointerEvent {
+                button: PointerEventButton::right,
+                kind: PointerEventKind::up,
+            } => self.state.right_mouse_held = false,
+            PointerEvent {
+                button: PointerEventButton::right,
+                kind: PointerEventKind::down,
+            } => self.state.right_mouse_held = true,
+            _ => (),
+        }
+    }
+}
